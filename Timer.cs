2a5172800605a23ce8cacd6f12b52d6ccc2fbf6c
sonyalx <151133335+sonyalx@users.noreply.github.com>
2024-01-17 22:17:07 +0300

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    public int minutes = 4;
+    private float seconds = 0f;
+
+    void Update()
+    {
+        if(seconds > 59) 
+        {
+            
+            if(minutes > 0) 
+            {
+                minutes--;
+            }
+
+            if(minutes == 0) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            
+            seconds = 0f;
+        } else {
+            seconds += Time.deltaTime;
+        }
+    }
+}
+
